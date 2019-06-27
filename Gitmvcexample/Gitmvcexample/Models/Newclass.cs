c3009484693e8ed29c61eb88ad4b3b7e20301d08
sindhusree58 <sindhusree58>
2019-06-27 14:18:10 +0530

    added

---
+++ Gitmvcexample/Gitmvcexample/Models/Newclass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gitmvcexample.Models
+{
+    public class Newclass
+    {
+        public int operation(int x, int y)
+        {
+            x = 5;
+            y = 3;
+           int c = x + y;
+            return c;
+        }
+    }
+}
